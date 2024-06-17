85b2125196093158e7e9a04ed34ae475b0abed1c
mishra06 <mishra.vibhakar7262@gmail.com>
2024-06-17 23:18:58 +0530

    frontend almost completed

--- frontend/twtr_frntend/src/components/Login.js
+++ frontend/twtr_frntend/src/components/Login.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [login , setLogin] = useState(true);
+  
+
+  const loginHandeler =()=>{
+        setLogin(!login);
+  }
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
     <div className='flex items-center justify-evenly w-[80%]'>
@@ -11,14 +18,19 @@ const Login = () => {
         <div className='my-5'>
           <h1 className='font-bold text-6xl'>Happening now.</h1>
         </div>
-        <h1 className='mt-4 mb-2 text-2xl font-bold'>hello</h1>
+        <h1 className='mt-4 mb-2 text-3xl font-bold'>{login ? "Sign in to X" : "Join today."}</h1>
         <form className='flex flex-col w-[55%]'>
-              <input type="text" placeholder='Name' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
-              <input type="text" placeholder='Username' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
+          {
+            !login && ( <>
+                  <input type="text" placeholder='Name' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
+                  <input type="text" placeholder='Username' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
+            </>)
+          }
+              
           <input type="email" placeholder='Email' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
           <input type="password" placeholder='Password' className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold" />
-          <button className='bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white'>Login</button>
-          <h1> <span className='font-bold text-blue-600 cursor-pointer'></span></h1>
+          <button className='bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white' >{login ? "Login" : "Singup"}</button>
+          <h1> {login ? "Do not have an account?" : "Already have an account?"}<span className='font-bold text-blue-600 cursor-pointer' onClick={loginHandeler}>{login ? "Signup" : "Login"}</span></h1>
         </form>
       </div>
     </div>
