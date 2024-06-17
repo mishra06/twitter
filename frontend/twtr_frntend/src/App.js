0c4e3e114a742e3c7df1db9cb96aa051329384a9
mishra06 <mishra.vibhakar7262@gmail.com>
2024-06-17 17:59:49 +0530

    rightSide updated and router setup

--- frontend/twtr_frntend/src/App.js
+++ frontend/twtr_frntend/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
+import Profile from './components/Profile';
+import Feed from './components/Feed';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                    <Route path="" element={<Feed />} />
+                    <Route path="/profile/:id" element={<Profile />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
   );
 }
 
--- frontend/twtr_frntend/src/components/RightSide.js
+++ frontend/twtr_frntend/src/components/RightSide.js
@@ -4,10 +4,27 @@ import Avatar from "react-avatar";
 
 const RightSide = () => {
   return (
-    <div className='w-[25%]'>
+    <div className='w-[25%] '>
       <div className='flex items-center p-2 bg-gray-100 rounded-full outline-none w-full'>
       <CiSearch size="25px" />
         <input type="text" className='bg-transparent outline-none px-2' placeholder='Search' />
+      </div>
+      <div className='p-4 bg-gray-100 rounded-2xl my-4'>
+        <h1 className='font-bold text-lg'>Who to follow</h1>
+        <div className='flex items-center justify-between my-3'>
+          <div className='flex'>
+            <div>
+              <Avatar src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" size="40" round={true} />
+            </div>
+            <div className='ml-2'>
+              <h1 className='font-bold'>Mishra</h1>
+              <p className='text-sm'>@Mishra</p>
+            </div>
+          </div>
+          <div>
+          <button className='px-4 py-1 bg-black text-white rounded-full'>Profile</button>
+          </div>
+        </div>
       </div>
     </div>
   )
